144b52b75c15357eea7f11e1364e54e230ec26b5
Aaacurse <rajputaakarsh2004@gmail.com>
2026-02-01 23:09:50 +0530

    Updated the test code

--- tests/test_opertions.py
+++ tests/test_opertions.py
@@ -3,7 +3,7 @@ from src.python_file import add,subtract
 def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
-    assert(-1,-1)==-2
+    assert add(-1,-1)==-2
 
 def test_subtract():
     assert subtract(5,3)==2
