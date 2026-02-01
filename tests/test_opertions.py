c68d49998e5d737d2e967218916d678b393a10be
Aaacurse <rajputaakarsh2004@gmail.com>
2026-02-01 23:05:59 +0530

    Updates test assertions

--- tests/test_opertions.py
+++ tests/test_opertions.py
@@ -7,6 +7,6 @@ def test_add():
 
 def test_subtract():
     assert subtract(5,3)==2
-    assert(2,2)==0
-    assert(1,-1)==2
-    assert(3,5)==-2
+    assert subtract(2,2)==0
+    assert subtract(1,-1)==2
+    assert subtract(3,5)==-2
